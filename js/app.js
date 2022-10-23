ce1e68bc87038ad9b8e47042d00089d1c1c2525a
mohamed hesham <hesmammohammed@gmail.com>
2022-10-23 11:33:58 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -1,40 +1,68 @@
 
-// counter to count  the  number of the new section
-let counter = 0;
-const createSection = () => {
-  counter++;
-  const section = `
-    <section id="section${counter}" data-nav="Section ${counter}">
-      <div class="landing__container">
-        <h2>Section ${counter}</h2>
 
-        <h3>Hello, this is the body for section ${counter} .</h3>
-        
+//create menu bar 
+//get all sections from the html file and add it by id to the navbar__list
+navMenu = document.getElementById("navbar__list");
+
+function createMenuItems () {
+  navMenu.innerHTML = "";
+  sections =  document.querySelectorAll("section")
+  for (section of sections){
+    sectionName = section.getAttribute("data-nav");
+    sectionId = section.getAttribute("id");
+    const listItem = `<li><a href="#${sectionId}" class="menu__link" data-active="${sectionId}">${sectionName}</a></li>`;
+    navMenu.innerHTML += listItem ;
+  }
+  
+};
+
+
+//function to add sections dynamiclly 
+function addSections (num) {
+
+  for (var next = 1; next < num; next++){
+
+    var section = `
+    <section id="section${next}" data-nav="Section ${next}">
+      <div class="landing__container">
+        <h2>Section ${next}</h2>
+
+        <h3>Hello, this is the body for section ${next} .</h3>
+      
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid fuga voluptatem quo perspiciatis atque totam optio earum quasi ullam laudantium, dicta pariatur maiores animi aut perferendis reprehenderit, nam similique dolorem.</p>
         <p>doloremque id culpa asperiores voluptas pariatur quasi aspernatur omnis corrupti. Quas in dolore numquam accusantium eos, voluptates magni blanditiis tempore doloremque harum ex nostrum distinctio neque. Iure culpa, dicta placeat quam dolor fugit, blanditiis assumenda neque aliquid quos, quibusdam consequatur. Porro magni fugit iste corrupti dicta esse quibusdam animi doloremque libero eum, cumque perspiciatis hic aspernatur iure autem necessitatibus et enim architecto illo atque doloribus asperiores id totam! Id maxime velit a earum harum consequatur minus similique, perspiciatis mollitia molestiae illum eligendi magnam quis!</p>
       
         </div>
     </section>`;
-  document.querySelector("main").insertAdjacentHTML("beforeend", section);
-};
-//create maneu bar 
-//get all sections from the html file and add it by id to the navbar__list
-const navBar = document.getElementById("navbar__list");
-const createNavItems = () => {
-  navBar.innerHTML = "";
-  document.querySelectorAll("section").forEach((section) => {
-    const listItem = `<li><a href="#${section.id}" data-nav="${section.id}" class="menu__link">${section.dataset.nav}</a></li>`;
-    navBar.insertAdjacentHTML("beforeend", listItem);
-  });
+    document.querySelector("main").insertAdjacentHTML("beforeend", section);
+
+    }
 };
 
-// create 5 section & create the nav bar items
-for (let i = 1; i < 6; i++) createSection();
-createNavItems();
 
+
+// I use getBoundingClientRect to get the postion of the section & test if it the current section ot not 
+// then add active class to the section & active menu to the navbar
+function isActive(){
+  for(section of sections){
+    let postion =  section.getBoundingClientRect();
+    console.log(postion.top)
+
+    if(postion.top <=250 && postion.top>= -300){
+      section.classList.add('active');
+      navMenu.querySelector(`[data-active=${section.getAttribute('id')}]`).classList.add("active-menu");
+    }
+    else{
+      section.classList.remove('active');
+      navMenu.querySelector(`[data-active=${section.getAttribute('id')}]`).classList.remove("active-menu");
+
+    }
+  }
+}
+// run th function while scrolling
+document.addEventListener("scroll",isActive);
 
 const topicon = document.getElementById("top");
-const header = document.querySelector(".page__header");
 
 // by Clicking on the top icon the page will scroll to the top smoothly
 topicon.addEventListener("click", () => {
@@ -42,54 +70,7 @@ topicon.addEventListener("click", () => {
 });
 
 
-
-//the header with disapear if the user stop scroll for 5 seconds & appear again when scrolling.
-//make the top icon appear if the user scroll more than 900
-let isScrolling;
-document.onscroll = () => {
-  header.style.display = "block"
-  clearTimeout(isScrolling)
-   isScrolling = setTimeout(() => {
-    header.style.display = "none";
-  }, 5000);
-
-  window.scrollY > 900 ? (topicon.style.display = "block"): (topicon.style.display = "none");
-};
-
-
-
-
-
-// I use getBoundingClientRect to test the postion of the section and get the current section on the viewport
-//then add active class to the section & active menu to the navbar
-window.onscroll = function() {
-	document.querySelectorAll("section").forEach(function(active) {
-    let activeMenuItem = navBar.querySelector(`[data-nav=${active.id}]`);
-    if(active.getBoundingClientRect().top >= -400 && active.getBoundingClientRect().top <= 150){
-
-      active.classList.add("active");
-      activeMenuItem.classList.add("active-menu");
-
-    }
-    else{
-      active.classList.remove("active");
-      activeMenuItem.classList.remove("active-menu");
-    }
-	});
-}
-
-
-
-navBar.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (event.target.dataset.nav) {
-    document
-      .getElementById(`${event.target.dataset.nav}`)
-      .scrollIntoView({ behavior: "smooth" });
-    setTimeout(() => {
-      location.hash = `${event.target.dataset.nav}`;
-    }, 200);
-  }
-});
-
+// functions call
+addSections(6);
+createMenuItems();
 
